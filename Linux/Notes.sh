7687ad620b8d32384513d825387b7cc09e78e05f
Azueon <44552609+Azueon@users.noreply.github.com>
2019-11-29 13:07:52 +0100

    Update Notes.sh

--- Linux/Notes.sh
+++ Linux/Notes.sh
@@ -19,19 +19,32 @@
 	ls -a 
 	#	Muestra contenido en la carpeta actual en un listado
 	ls -l
+	#	Muestra contenido en la carpeta actual (incluyendo archivos ocultos)
+	ls -laxo
+	#   Muestra contenido en la carpeta actual pero filtrando por tipo de archivo que indiquemos
+	ls *.txt
 	#	[!] Muestra la ruta actual (donde está el foco en ese momento)
 	pwd 
 
 
-# - - - - - Para Copiar, Eliminar, Renombrar, Cortar archivos o carpetas:
+# - - - - - Para Crear, Copiar, Eliminar, Renombrar, Cortar archivos o carpetas:
+	#   Para crear una carpeta en la ruta donde estamos:
+	mkdir NombreCarpeta
+	#   Para eliminar una carpeta y su contenido:
+	rmdir NombreCarpeta
 	#	[!] Para eliminar un archivo (pero no una carpeta) (Si necesita permiso añada "sudo" delante de "rm")
 	rm nombrearchivo.extensionarchivo
-	#	Para eliminar un archivo o carpeta y todas sus subcarpetas
+	#	Para eliminar un archivo o carpeta y todas sus subcarpetas:
 	rm -r nombrearchivo
-	#	Renombra un archivo:
+	#	Renombra un archivo o moverlo a una ruta nueva:
 	mv nombreantiguo.extension nuevonombre.extension
-	#	Para copiar un archivo de un lugar a otro
+	mv nombreantiguo.extension ruta
+	#	Para copiar un archivo de un lugar a otro:
 	sudo cp proxy.sh /etc/init.d
+	#	Para eliminar todos los archivos de la carpeta actual:
+	sudo rm *
+	#	Para eliminar todos los archivos de la carpeta actual incluyendo archivos y subcarpetas:
+	sudo rm *
 
 # - - - - - Guardar salida en un fichero de texto:
 	#	Guardar salida en un fichero de texto:
@@ -119,24 +132,26 @@
 #	1. Creamos en la siguiente ruta este archivo:
 	sudo nano /etc/init.d/proxy.sh
 #	2. Escribimos dentro de ese archivo el siguiente código:
-	#~ !/bin/bash
-	IPTABLES="/sbin/iptables"
-	#~ Limpiamos las reglas anteriores y ponemos los contadores a cero:
-	$IPTABLES -F
-	$IPTABLES -t nat -F
-	$IPTABLES -Z
-	#~ Establecemos la política por defecto de las cadenas de la tabla
-	#~ filter
-	$IPTABLES -P INPUT ACCEPT
-	$IPTABLES -P OUTPUT ACCEPT
-	$IPTABLES -P FORWARD ACCEPT
-	$IPTABLES -t nat -P PREROUTING ACCEPT
-	$IPTABLES -t nat -P POSTROUTING ACCEPT
-	/sbin/iptables -P FORWARD ACCEPT
-	#~iptables -t nat -A PREROUTING -i eth1 -p tcp --dport 80 -j REDIRECT --to-port 8080
-	/sbin/iptables --table nat -A POSTROUTING -s 172.16.0.0/16 -o eth0 -j MASQUERADE
-	#~/sbin/iptables -t nat -A PREROUTING -p tcp -i eth0 --dport 80 -j DNAT --to-destination 172.17.0.75:80
-	echo 1 > /proc/sys/net/ipv4/ip_forward
+
+				#~ !/bin/bash
+				IPTABLES="/sbin/iptables"
+				#~ Limpiamos las reglas anteriores y ponemos los contadores a cero:
+				$IPTABLES -F
+				$IPTABLES -t nat -F
+				$IPTABLES -Z
+				#~ Establecemos la política por defecto de las cadenas de la tabla
+				#~ filter
+				$IPTABLES -P INPUT ACCEPT
+				$IPTABLES -P OUTPUT ACCEPT
+				$IPTABLES -P FORWARD ACCEPT
+				$IPTABLES -t nat -P PREROUTING ACCEPT
+				$IPTABLES -t nat -P POSTROUTING ACCEPT
+				/sbin/iptables -P FORWARD ACCEPT
+				#~iptables -t nat -A PREROUTING -i eth1 -p tcp --dport 80 -j REDIRECT --to-port 8080
+				/sbin/iptables --table nat -A POSTROUTING -s 172.16.0.0/16 -o eth0 -j MASQUERADE
+				#~/sbin/iptables -t nat -A PREROUTING -p tcp -i eth0 --dport 80 -j DNAT --to-destination 172.17.0.75:80
+				echo 1 > /proc/sys/net/ipv4/ip_forward
+
 #	3. Una vez guardado lo anterior para ejecutarlo usaremos
 	sudo sh /etc/init.d/proxy.sh 
 #	4. Finalmente deberemos incorporarlo en un enlace simbólico
@@ -162,29 +177,31 @@
 #	Dentro del archivo de configuracion podemos usar las variables globales
 #	que daran una configuracion por defecto o podemos por ejemplo usar 
 #	esta configuracion:
-#	A slightly diferent configuration for an internar subnet.
-	subnet 172.10.0.0 netmask 255.255.0.0 {
-		range 172.16.0.50 172.16.0.100;
-		option domain-name-servers 192.168.8.1, 192.169.8.2, 8.8.8.8;
-		option domain-name "daemon.lan";
-		option routers 172.16.0.1;
-		option broadcast-address 172.16.255.255;
-		default-lease-time 600;
-		max-lease-time 7200;
-		interface eth1;
-	}
 
-	subnet 172.16.0.0 netmask 255.255.0.0 {
-		option routers 172.16.10.1;
-		option subnet-mask 255.255.0.0;
-		option domain-name-servers 192.168.8.1, 192.169.8.2;
-		option domain-name "daemon.lan";
-		range 172.16.0.10 172.16.0.100;
-		option broadcast-address 172.16.255.255;
-		default-lease-time 600;
-		max-lease-time 7200;
-		interface eth1;
-	}	
+			#	A slightly diferent configuration for an internar subnet.
+				subnet 172.10.0.0 netmask 255.255.0.0 {
+					range 172.16.0.50 172.16.0.100;
+					option domain-name-servers 192.168.8.1, 192.169.8.2, 8.8.8.8;
+					option domain-name "daemon.lan";
+					option routers 172.16.0.1;
+					option broadcast-address 172.16.255.255;
+					default-lease-time 600;
+					max-lease-time 7200;
+					interface eth1;
+				}
+
+				subnet 172.16.0.0 netmask 255.255.0.0 {
+					option routers 172.16.10.1;
+					option subnet-mask 255.255.0.0;
+					option domain-name-servers 192.168.8.1, 192.169.8.2;
+					option domain-name "daemon.lan";
+					range 172.16.0.10 172.16.0.100;
+					option broadcast-address 172.16.255.255;
+					default-lease-time 600;
+					max-lease-time 7200;
+					interface eth1;
+				}	
+
 #	Para arrancar el servicio:
 	sudo service isc-dhcp-server restart
 
@@ -204,7 +221,7 @@
 #	Comandos para DNS
 #	[!] Devuelve la ip de esta maquina:
 	nslookup nombredominio
-#	Instalar un servidor DNS:
+#	Instalar un servidor DNS Maestro:
 #	1. Antes de nada configura las interfaces de red y el servidor dhcp para que se
 #	emita correctamente y no haya problemas con otros servidores de nombres
 	sudo nano /etc/network/interfaces
@@ -213,14 +230,16 @@
 	cd /etc/bind/
 	sudo nano named.conf.local
 # 	Editamos named.conf.local:
-	zone "nombredeldominio.es" {
-		type master;
-		file "/etc/bind/db.nombredeldominio";
-	};
-	zone "17.172.in_addr.arpa" {
-		type master;
-		file "/etc/bind/db.172";
-	};
+
+				zone "nombredeldominio.es" {
+					type master;
+					file "/etc/bind/db.nombredeldominio";
+				};
+				zone "17.172.in_addr.arpa" {
+					type master;
+					file "/etc/bind/db.172";
+				};
+
 #	Creamos el fichero de configuración "db.nombredeldominio" a partir de “db.local”:
 	cp db.local db.nombredeldominio
 #	Editamos “db.nombredeldominio:
@@ -228,26 +247,29 @@
 #   reemplazamos la palabra “localhost” pornombredeldominio.homeip.net”, cambiamos la
 #   IP “127.0.0.1″ por la que queramos asignar al dominio y añadimos al final
 #	del fichero todos los A, MX y CNAME que queramos, quedando:
-	; BIND data file for local loopback interface
-	$TTL 604800
-	@ IN SOA nombredeldominio.es. root.nombredeldominio.es. (
-	 2 ; Serial
-	 604800 ; Refresh
-	 86400 ; Retry
-	 2419200 ; Expire
-	 604800 ) ; Negative Cache TTL
-	;
-	@ IN NS nombredeldominio.es.
-	@ IN A 172.17.0.1
-	www IN A 172.17.0.2
-	;mail IN MX 0 192.168.15.4
-	ftp IN A 172.17.0.3
-	files IN A 172.17.0.4
-	;mail IN CNAME mail.nombredeldominio.es.
-	;ftp IN CNAME ftp.nombredeldominio.es.
-	;files IN CNAME files.nombredeldominio.es.
-	;www IN CNAME www.nombredeldominio.es.
-#	Cada vez que se cambia la configuración de BIND9, debemos reiniciar el demonio:
+
+				; BIND data file for local loopback interface
+				$TTL 604800
+				@ IN SOA nombredeldominio.es. root.nombredeldominio.es. (
+				 2 ; Serial
+				 604800 ; Refresh
+				 86400 ; Retry
+				 2419200 ; Expire
+				 604800 ) ; Negative Cache TTL
+				;
+				@ IN NS nombredeldominio.es.
+				@ IN A 172.17.0.1
+				dns IN A 172.17.0.1
+				www IN A 172.17.0.2
+				;mail IN MX 0 192.168.15.4
+				ftp IN A 172.17.0.3
+				files IN A 172.17.0.4
+				;mail IN CNAME mail.nombredeldominio.es.
+				;ftp IN CNAME ftp.nombredeldominio.es.
+				;files IN CNAME files.nombredeldominio.es.
+				;www IN CNAME www.nombredeldominio.es.
+
+#	3. Cada vez que se cambia la configuración de BIND9, debemos reiniciar el demonio:
 	/etc/init.d/bind9 restart
 #	Para que nuestra máquina utilice el servidor de DNS que hemos configurado,
 #	debemos editar “/etc/resolv.conf” y dejamos únicamente la línea:
@@ -257,15 +279,22 @@
 #	127.0.0.1 por la IP que tenga el servidor en la red.
 #	Para comprobar el correcto funcionamiento, utilizamos el comando “host” el
 #	cual sirve para resolver dominios:
-	$ host marblestation.homeip.net
-	marblestation.homeip.net has address 192.168.48.32
-	marblestation.homeip.net mail is handled by 0 marblestation.homeip.net.
-	$ host saturno.marblestation.homeip.net
-	saturno.marblestation.homeip.net is an alias for marblestation.homeip.net.
-	marblestation.homeip.net has address 192.168.48.32
-	saturno.marblestation.homeip.net is an alias for marblestation.homeip.net.
-	saturno.marblestation.homeip.net is an alias for marblestation.homeip.net.
-	marblestation.homeip.net mail is handled by 0 marblestation.homeip.net.
+
+				$ host marblestation.homeip.net
+				marblestation.homeip.net has address 192.168.48.32
+				marblestation.homeip.net mail is handled by 0 marblestation.homeip.net.
+				$ host saturno.marblestation.homeip.net
+				saturno.marblestation.homeip.net is an alias for marblestation.homeip.net.
+				marblestation.homeip.net has address 192.168.48.32
+				saturno.marblestation.homeip.net is an alias for marblestation.homeip.net.
+				saturno.marblestation.homeip.net is an alias for marblestation.homeip.net.
+				marblestation.homeip.net mail is handled by 0 marblestation.homeip.net.
+
 # Para iniciar el servidor bin9:
 	sudo service bin9 restart
-
+# Para parar el servidor bin9:
+	sudo service bin9 stop
+# Para ver la carpeta donde se guarda la cache del servidor bin9:
+	sudo ls /var/cache/bind
+#	Para verificar si esta correcta la configuracion:
+named-checkzone nombredeldominio.es db.nombredeldominio
